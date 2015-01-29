dd0aca6856132691ac9bc7923dbbe5f4bbcce7ba
Tim <Tim@192.168.178.23>
2015-01-29 19:27:37 +0100

    updating the demo_app

--- demo_app/app.js
+++ demo_app/app.js
@@ -1,75 +1,59 @@
-(function(window) {
+(function (window) {
     var angular = window.angular;
     angular.module('demoApp', ['ngResource'])
             .factory('demo_zone', [
                 '$resource',
-                function($resource) {
-                    //Makes a Resource and defines a custom method (update)
-                    return $resource('/resful_api/www/demo_zone/user/:userId', {
-//                        userId: "@userId"
-                    }, {
-                        "update": {
-                            method: "PUT"
-                        },
-                        "reviews": {
-                            'method': 'GET',
-                            isArray: true
-                        }
+                function ($resource) {
+                    //Makes a Resource and defines a custom method (update) and (add)
+                    return $resource('/resful_api/www/demo_zone/user/:userId', {}, {
+                        "update": {method: "PUT"},
+                        "add": {'method': 'POST'}
                     });
                 }
             ])
             .controller('AppController', [
                 '$scope',
                 'demo_zone',
-                function($scope, Demo_zone) {
+                function ($scope, Demo_zone) {
                     $scope.log = [];
 
-//                    var bookings = Demo_zone.query(); // Calls: GET /api/booking/
+                    //---------------  A GET ONE AND PUT ONE DEMO
+                    var demoOne = Demo_zone.get({'userId': 1}); // Calls: GET 
+                    demoOne.name = 'Steven Smith';
+                    demoOne.$update({'userId': 1}); // Calls: PUT
+                    demoOne.$promise.then(function (a) {
+                        $scope.log.push('A GET ONE AND PUT ONE DEMO ->');
+                        $scope.log.push('After GET and then PUT, the PUT Response: ' + angular.toJson(a));
+                        $scope.log.push('-- BREAK --');
+//                        console.log('GET AND PUT: ', angular.toJson(a));
+                    });
 
-// Get Booking ID 1
-//                    var booking = Demo_zone.get({}, {'Id': 1}); // Calls: GET /api/booking/1
+
+                    //---------------  A GET ALL DEMO
+                    var demoTwo = Demo_zone.query(); // Calls: GET
+                    demoTwo.$promise.then(function (a) {
+                        $scope.log.push('A GET ALL DEMO ->');
+                        $scope.log.push('After GET the Response: ' + angular.toJson(a));
+                        $scope.log.push('-- BREAK --');
+                    });
 
 
-                    var booking = Demo_zone.get({'userId': 1}); // Calls: GET /api/booking/1
-                    booking.name = 'Steven Smith';
-                    booking.$update({'userId': 1});
+                    //---------------  A DELETE ONE DEMO
+                    var demoThree = Demo_zone.delete({'userId': 1}); // Calls: DELETE
+                    demoThree.$promise.then(function (a) {
+                        $scope.log.push('A DELETE ONE DEMO ->');
+                        $scope.log.push('After DELETE the Response: ' + angular.toJson(a));
+                        $scope.log.push('-- BREAK --');
+                    });
 
-                    
 
-
-// Change a value
-//                    booking.fees = 34;
-//                    console.log(booking);
-//                    Demo_zone.update({Id: 1}, {a: 5});
-// Save the changes (update it)
-//                    booking.$save(); // Calls: POST /api/booking/1
-
-// Delete Booking ID 1
-//                    Demo_zone.delete({}, {'Id': 1}); // Calls: DELETE /api/booking/1
-
-// Get Reviews
-//                    var reviews = Demo_zone.reviews();
-
-//                    var demo = Demo_zone.get({
-//                        id: 1234,
-//                        type: 'demoType'
-//                    }, function(a) {
-//                        $scope.log.push('GET /resful_api/www/demo_zone/demoType/1234');
-//                        $scope.log.push('Response: ' + angular.toJson(a));
-//                    });
-//
-//
-//
-//                    demo.name = 'Testname';
-//                    demo.$save();
-
-//                    Demo_zone.update({
-//                        id: 1234,
-//                        type: 'demoType'
-//                    }, demo, function(response) {
-//                        $scope.log.push('PUT /resful_api/www/demo_zone/demoType/1234');
-//                        $scope.log.push('Response: ' + angular.toJson(response));
-//                    });
+                    //---------------  A ADD ONE DEMO
+                    var demoFour = Demo_zone.add({'userId': 2}, {name: 'someone'});
+                    demoFour.$promise.then(function (a) {
+                        $scope.log.push('A ADD ONE DEMO ->');
+                        $scope.log.push('After POST the Response: ' + angular.toJson(a));
+                        $scope.log.push('-- BREAK --');
+                    });
                 }
             ]);
 })(window);
